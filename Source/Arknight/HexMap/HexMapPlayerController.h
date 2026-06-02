a509c6b8afe5c0b4f117b87d623b2a573d4fa91a
IDHtan <tanyuxuan@sjtu.edu.cn>
2026-06-02 15:38:23 +0800

    module: add hexmapsubsystem and pc

---
+++ Source/Arknight/HexMap/HexMapPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HexMapPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKNIGHT_API AHexMapPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
