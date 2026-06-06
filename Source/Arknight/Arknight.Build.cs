e6e4d4d75c2f309bff04743f9349ac56f5fb9838
IDHtan <tanyuxuan@sjtu.edu.cn>
2026-06-07 03:33:28 +0800

    debug: remove the plugin without usage

--- Source/Arknight/Arknight.Build.cs
+++ Source/Arknight/Arknight.Build.cs
@@ -8,7 +8,7 @@ public class Arknight : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "SpinePlugin" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
